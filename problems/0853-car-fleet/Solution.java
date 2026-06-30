7a3f64224ad71bea60af571dde2cad56352c260c
Abhimanyu Guleria <abhimanyuguleriaixf@gmail.com>
2026-06-30 06:07:43 +0000

    Solve: 853. Car Fleet (Java)

--- problems/0853-car-fleet/Solution.java
+++ problems/0853-car-fleet/Solution.java
@@ -1,37 +1,26 @@
 class Solution {
-
     public int carFleet(int target, int[] position, int[] speed) {
-
         double[][] cars = new double[position.length][2];
 
-        for(int i = 0; i < position.length; i++){
-
+        for(int i = 0; i <position.length; i++){
             cars[i][0] = position[i];
-
-            cars[i][1] = (double)(target - position[i]) / speed[i];
-
+            cars[i][1] = (double)(target - position[i])/speed[i];
         }
 
         Arrays.sort(cars, (a,b) -> Double.compare(a[0], b[0]));
 
-        int fleets = 0;
-
+        int fleet = 0;
         double fleetTime = 0;
 
-        for(int i = cars.length-1; i>=0; i--){
-
+        for(int i= position.length-1; i>=0;i--){
             double currentTime = cars[i][1];
 
             if(currentTime > fleetTime){
-
-                fleets++;
-
+                fleet++;
                 fleetTime = currentTime;
-
             }
-
         }
+        return fleet;
 
-        return fleets;
     }
 }
